3fe403d999b90b51330400a530157ac06307b21e
MWisBest <repinski23@gmail.com>
2013-05-11 13:00:57 -0500

    This should be it.

--- su/dbstub.c
+++ su/dbstub.c
@@ -1,11 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
-#include <sqlite3.h>
-#include <time.h>
 
 #include "../../../system/su/su.h"
 
-policy_t database_check(struct su_context *ctx) {
+int database_check(struct su_context *ctx) {
     return ALLOW;
 }
